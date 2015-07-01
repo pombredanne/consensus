e4df860644a41e109711bc03d70045c82b0ae637
Hephaestos <hephaestos@riseup.net>
2015-07-01 11:32:49 +0000

    Create consensus_list.py

---
+++ consensus_list.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+consensus_list = [
+	('Bootstrap (css/js/fonts)', '3.3.5', 'source code', 'https://github.com/twbs/bootstrap/releases/download/v3.3.5/bootstrap-3.3.5-dist.zip'),
+	('creatorrc', 'May 2nd', 'source code', 'https://github.com/hephaest0s/creatorrc/archive/23fd1031806ddd46dfad98727ea6a5690ebd15d9.zip'),
+	('gpg4usb', '0.3.3-1', 'win/linux', 'http://www.gpg4usb.org/gpg4usb-0.3.3-1.zip'),
+	('HTTPS Everywhere', '2015.5.28', 'Chrome, Chromium, Opera', 'https://www.eff.org/files/https-everywhere-chrome-2015.5.28.crx'),
+	('HTTPS Everywhere', '5.05', 'firefox', 'https://www.eff.org/files/https-everywhere-5.0.5.xpi'),
+	('jQuery', '2.1.4.min', 'source code', 'http://code.jquery.com/jquery-2.1.4.min.js'),
+	('kdf', 'May 2nd', 'source code', 'https://github.com/hephaest0s/kdf/archive/18c2c9b3e850a6a83fd220a7bf736e59dd6311a1.zip'),
+	('usbkill', '1.0-rc.2', 'source code', 'https://github.com/hephaest0s/usbkill/archive/a90aa1e166422ec8af00ee36adacd67ba4f79f7b.zip'),
+	]
