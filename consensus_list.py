b7779aa34f7d547f5632f574fca130bda998c3b8
Hephaestos <hephaestos@riseup.net>
2015-07-29 11:39:31 +0000

    Update consensus_list.py

--- consensus_list.py
+++ consensus_list.py
@@ -4,9 +4,9 @@ consensus_list = [
 	('Bootstrap (css/js/fonts)', '3.3.5', 'source code', 'https://github.com/twbs/bootstrap/releases/download/v3.3.5/bootstrap-3.3.5-dist.zip'),
 	('creatorrc', 'May 2nd', 'source code', 'https://github.com/hephaest0s/creatorrc/archive/23fd1031806ddd46dfad98727ea6a5690ebd15d9.zip'),
 	('gpg4usb', '0.3.3-1', 'win/linux', 'http://www.gpg4usb.org/gpg4usb-0.3.3-1.zip'),
-	('HTTPS Everywhere', '2015.5.28', 'Chrome, Chromium, Opera', 'https://www.eff.org/files/https-everywhere-chrome-2015.5.28.crx'),
-	('HTTPS Everywhere', '5.05', 'firefox', 'https://www.eff.org/files/https-everywhere-5.0.5.xpi'),
+	('HTTPS Everywhere', '2015.5.28', 'Chrome, Chromium, Opera', 'https://www.eff.org/files/https-everywhere-chrome-2015.7.17.crx'),
+	('HTTPS Everywhere', '5.05', 'firefox', 'https://www.eff.org/files/https-everywhere-5.0.7.xpi'),
 	('jQuery', '2.1.4.min', 'source code', 'http://code.jquery.com/jquery-2.1.4.min.js'),
 	('kdf', 'May 2nd', 'source code', 'https://github.com/hephaest0s/kdf/archive/18c2c9b3e850a6a83fd220a7bf736e59dd6311a1.zip'),
-	('usbkill', '1.0-rc.2', 'source code', 'https://github.com/hephaest0s/usbkill/archive/a90aa1e166422ec8af00ee36adacd67ba4f79f7b.zip'),
+	('usbkill', '1.0-rc.4', 'source code', 'https://github.com/hephaest0s/usbkill/archive/9443a2a87ecdeb004b0be9fdc41ec049a56ab073.zip'),
 	]
